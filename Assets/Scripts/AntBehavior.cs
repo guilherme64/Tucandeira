42641b6a3eed0114a09c1aa6e87313ab26a6db1f
Guilherme <guilhermemelo6@outlook.com>
2019-04-08 11:42:40 -0300

    Pushing this game to github

---
+++ Assets/Scripts/AntBehavior.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntBehavior : MonoBehaviour {
+	float speed = 0.2f;
+	public int direction = 1;
+	GameObject target;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direction == 1) {
+			gameObject.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y + speed, gameObject.transform.position.z);
+			//gameObject.transform.localScale = new Vector3(-gameObject.transform.localScale.x, gameObject.transform.localScale.y+ speed, gameObject.transform.localScale.z );
+		} else if (direction == 2) {
+			gameObject.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, -90));
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x + speed, gameObject.transform.position.y, gameObject.transform.position.z);
+		}else if (direction == 3) {
+			gameObject.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 180));
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y - speed, gameObject.transform.position.z);
+		}else if (direction == 4) {
+			gameObject.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 90));
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x - speed, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "Scenario") {
+			if (direction == 1) {
+				if (Random.value <= 0.5) {
+					direction = 4;
+				} else {
+					direction = 2;
+				}
+			} else if (direction == 2) {
+				if (Random.value <= 0.5) {
+					direction = 1;
+				} else {
+					direction = 3;
+				}
+			}else if (direction == 3) {
+				if (Random.value <= 0.5) {
+					direction = 4;
+				} else {
+					direction = 2;
+				}
+			}else if (direction == 4) {
+				if (Random.value <= 0.5) {
+					direction = 1;
+				} else {
+					direction = 3;
+				}
+			}
+		}
+
+		if (other.gameObject.tag == "Ant" ||other.gameObject.tag == "gb" ) {
+			Physics2D.IgnoreCollision (other.gameObject.GetComponent<BoxCollider2D> (), gameObject.GetComponent<BoxCollider2D> ());
+		}
+	}
+}
---
+++ Assets/Scripts/MoveBehavior.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBehavior : MonoBehaviour {
+	int gbNumber = 0;
+	float speed = 0.1f;
+	bool goUp;
+	bool goDown;
+	bool goLeft;
+	bool goRight;
+	public bool wasSeen = false;
+	public Animator _anim;
+	// Use this for initialization
+	void Start () {
+		
+		goRight = true;
+		goUp	= true;
+		goDown	= true;
+		goLeft	= true;
+		_anim = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.W)) {
+			_anim.SetBool ("UP", true);//gambiarra feia do caralho, conserta essa porra depois
+			_anim.SetBool ("DOWN", false);
+			_anim.SetBool ("LEFT", false);
+			_anim.SetBool ("RIGHT", false);
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y + speed, gameObject.transform.position.z);
+
+		} else if (Input.GetKey (KeyCode.S)) {
+			if (goDown) {
+				//_anim.SetTrigger("Down");
+				_anim.SetBool ("DOWN", true);
+				_anim.SetBool ("UP", false);//gambiarra feia do caralho, conserta essa porra depois
+				_anim.SetBool ("LEFT", false);
+				_anim.SetBool ("RIGHT", false);
+				//gameObject.GetComponent<Animator> ().SetBool("Down", true);
+				gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y - speed, gameObject.transform.position.z);
+			}
+		} else if (Input.GetKey (KeyCode.A)) {
+			if (goLeft == true) {
+				//_anim.SetTrigger("Left");
+				_anim.SetBool ("LEFT", true);
+				_anim.SetBool ("UP", false);//gambiarra feia do caralho, conserta essa porra depois
+				_anim.SetBool ("DOWN", false);
+				_anim.SetBool ("RIGHT", false);
+				//gameObject.GetComponent<Animator> ().SetBool("Left", true);
+				gameObject.transform.position = new Vector3 (gameObject.transform.position.x - speed, gameObject.transform.position.y, gameObject.transform.position.z);
+			}
+		} else if (Input.GetKey (KeyCode.D)) {
+			if (goLeft) {
+				//_anim.SetTrigger("Right");	
+				_anim.SetBool ("RIGHT", true);
+				_anim.SetBool ("UP", false);//gambiarra feia do caralho, conserta essa porra depois
+				_anim.SetBool ("DOWN", false);
+				_anim.SetBool ("LEFT", false);
+
+				//gameObject.GetComponent<Animator> ().SetBool("Right", true);
+				gameObject.transform.position = new Vector3 (gameObject.transform.position.x + speed, gameObject.transform.position.y, gameObject.transform.position.z);
+			}
+		} else {
+			_anim.SetTrigger("Stand");
+			_anim.SetBool ("UP", false);//gambiarra feia do caralho, conserta essa porra depois
+			_anim.SetBool ("DOWN", false);
+			_anim.SetBool ("LEFT", false);
+			_anim.SetBool ("RIGHT", false);
+			//gameObject.GetComponent<Animator> ().SetBool("Up", false);
+			//gameObject.GetComponent<Animator> ().SetBool("Right", false);
+			//gameObject.GetComponent<Animator> ().SetBool("Left", false);
+			//gameObject.GetComponent<Animator> ().SetBool("Down", false);
+			//gameObject.GetComponent<Animator> ().SetTrigger("Stand");
+		} /*else {
+			gameObject.GetComponent<Animator>().SetBool("WisPressed", false);
+			gameObject.GetComponent<Animator>().SetBool("SisPressed", false);
+			gameObject.GetComponent<Animator>().SetBool("AisPressed", false);
+			gameObject.GetComponent<Animator>().SetBool("DisPressed", false);
+		}*/
+	}
+
+
+	void OnCollisionEnter2D(Collision2D other){
+		Debug.Log ("bateu");
+		if (other.gameObject.tag == "Ant") {
+			Destroy (gameObject);
+			LoadScene (1);
+		}
+		if (other.gameObject.tag == "gb") {
+			Destroy (other.gameObject);
+
+		}
+	}
+
+	public void LoadScene(int level){
+		Application.LoadLevel(level);
+	}
+}
